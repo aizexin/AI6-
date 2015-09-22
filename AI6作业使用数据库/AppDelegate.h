b8235e3da4caf511cdd7566e7fe935e56e1cbce7
aizexin <443792431@qq.com>
2015-09-22 19:19:37 +0800

    Initial Commit

---
+++ AI6作业使用数据库/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AI6作业使用数据库
+//
+//  Created by qianfeng on 15/9/22.
+//  Copyright (c) 2015年 aizexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AI6作业使用数据库/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AI6作业使用数据库
+//
+//  Created by qianfeng on 15/9/22.
+//  Copyright (c) 2015年 aizexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
